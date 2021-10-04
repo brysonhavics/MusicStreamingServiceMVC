c4a392bd711d59925e368d1b3a0216c8b306250f
brysonhavics <87788293+brysonhavics@users.noreply.github.com>
2021-10-04 17:16:36 -0400

    Added create models

---
+++ MusicStreamingService.Models/ArtistsModels/ArtistCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStreamingService.Models.ArtistsModels
+{
+    public class ArtistCreate
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(200, ErrorMessage = "Too many characters, please shorten.")]
+        public string About { get; set; }
+        [Required]
+        public DateTime Birthday { get; set; }
+    }
+}

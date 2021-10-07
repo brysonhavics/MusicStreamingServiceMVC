6534dd33eb77fb96a4f84b08759ac93b9dd68ccc
brysonhavics <87788293+brysonhavics@users.noreply.github.com>
2021-10-07 16:04:00 -0400

    Song work

---
+++ MusicStreamingService.Models/SongModels/SongListItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStreamingService.Models.SongModels
+{
+    public class SongListItem
+    {
+        public int SongId { get; set; }
+        public string AlbumName { get; set; }
+        public string ArtistName { get; set; }
+        public string Name { get; set; }
+    }
+}
